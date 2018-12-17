3ec784cc42c87e40fff9be55dbbc0270d95073d6
clementchenebault <clem.chenebault@gmail.com>
2018-12-17 09:57:27 +0000

    change urls

--- react-slider/precache-manifest.b1a12587b4a2237f8c74b6424d0018cc.js
+++ react-slider/precache-manifest.b1a12587b4a2237f8c74b6424d0018cc.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = [
   {
     "revision": "e101493194815493a7c5f4f51b6da206",
-    "url": "/static/media/loading-mark.e1014931.svg"
+    "url": "https://madclem.github.io/react-slider/static/media/loading-mark.e1014931.svg"
   },
   {
     "revision": "229c360febb4351a89df",
-    "url": "/static/js/runtime~main.229c360f.js"
+    "url": "https://madclem.github.io/react-slider/static/js/runtime~main.229c360f.js"
   },
   {
     "revision": "44040c851049bc754d28",
-    "url": "/static/js/main.44040c85.chunk.js"
+    "url": "https://madclem.github.io/react-slider/static/js/main.44040c85.chunk.js"
   },
   {
     "revision": "394d8c5f51deb16de3ab",
-    "url": "/static/js/1.394d8c5f.chunk.js"
+    "url": "https://madclem.github.io/react-slider/static/js/1.394d8c5f.chunk.js"
   },
   {
     "revision": "44040c851049bc754d28",
-    "url": "/static/css/main.c580d842.chunk.css"
+    "url": "https://madclem.github.io/react-slider/static/css/main.c580d842.chunk.css"
   },
   {
     "revision": "f09c50aca04bb8a77fe2e2670ca2b59d",
-    "url": "/index.html"
+    "url": "https://madclem.github.io/react-slider/index.html"
   }
 ];
