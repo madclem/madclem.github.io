b505ab05ad986f3e8a57468ff3b2a5ad874b9d84
clementchenebault <clem.chenebault@gmail.com>
2018-12-17 11:02:30 +0000

    update slider to make it work on mobile

---
+++ react-slider/precache-manifest.794f76523f9ab37129345cab2fcac002.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e101493194815493a7c5f4f51b6da206",
+    "url": "/react-slider/static/media/loading-mark.e1014931.svg"
+  },
+  {
+    "revision": "cbfa59d94e626f526896",
+    "url": "/react-slider/static/js/runtime~main.cbfa59d9.js"
+  },
+  {
+    "revision": "7293dda8d369b54c0ade",
+    "url": "/react-slider/static/js/main.7293dda8.chunk.js"
+  },
+  {
+    "revision": "394d8c5f51deb16de3ab",
+    "url": "/react-slider/static/js/1.394d8c5f.chunk.js"
+  },
+  {
+    "revision": "7293dda8d369b54c0ade",
+    "url": "/react-slider/static/css/main.30aa45c5.chunk.css"
+  },
+  {
+    "revision": "68afed98cea17d5763c3851e721e591b",
+    "url": "/react-slider/index.html"
+  }
+];
