48dc2a709ad843075e4963be685e477902f24e85
clementchenebault <clem.chenebault@gmail.com>
2019-06-20 21:07:15 +0100

    nyan pig wip

---
+++ nyan-pig/precache-manifest.a3e2468f6274373d1c5c6c32df38a027.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce0c5c17b7e8c86196935c6a5b52cf98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce494bff4a5f3f104ff2",
+    "url": "/static/css/main.f5981296.chunk.css"
+  },
+  {
+    "revision": "b7844e7d59053345af0e",
+    "url": "/static/js/2.2c3f1b5e.chunk.js"
+  },
+  {
+    "revision": "ce494bff4a5f3f104ff2",
+    "url": "/static/js/main.235ce116.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
