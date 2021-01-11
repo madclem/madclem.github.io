f2a4884b9de969eaff22842844a7635019206e12
Clement <clem.chenebault@gmail.com>
2021-01-11 09:41:20 +0100

    orbs

--- black-and-white/01/assets/js/config.js
+++ black-and-white/01/assets/js/config.js
@@ -6,9 +6,9 @@ var WEBGL_CONFIG = {
   NAME_BUILD: "WebGLAnimation",
   SOUND: false,
   START_BUTTON: false,
-  TITLE: "B&W - Orbs",
+  TITLE: "Orbs",
   INSTRUCTIONS: "",
-  DESCRIPTION: "WebGL Lines",
+  DESCRIPTION: "Black & white, points & line",
   COLOR: "#d4d4d4",
   BACKGROUND_COLOR:
     "radial-gradient(circle, rgba(54,54,54,1) 0%, rgba(40,39,39,1) 28%, rgba(0,0,0,1) 93%)",
