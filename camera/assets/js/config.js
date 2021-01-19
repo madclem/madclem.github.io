db5900085b3efc5ea6407f24b0a61a4242484de7
Clement <clem.chenebault@gmail.com>
2021-01-19 11:38:04 +0000

    camera

---
+++ camera/assets/js/config.js
@@ -0,0 +1,16 @@
+var WEBGL_CONFIG = {
+  SHOW_LOADER_FAKE: false,
+  MIN_LOADING_TIME: 0,
+  DELAY_FADEOUT_INFOS: 0,
+  ASSETS_PATH: "assets/",
+  NAME_BUILD: "WebGLAnimation",
+  SOUND: false,
+  START_BUTTON: false,
+  TITLE: "",
+  INSTRUCTIONS: "",
+  DESCRIPTION: "",
+  COLOR: "white",
+  BACKGROUND_COLOR:
+    "linear-gradient(0deg, rgba(248,181,128,1) 0%, rgba(100,96,115,1) 40%, rgba(100,96,115,1) 60%, rgba(248,181,128,1) 100%)",
+  CREDITS: [],
+};
