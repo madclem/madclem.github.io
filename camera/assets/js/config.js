b22c6a020a3f884a1b830a59802f5f387e58c975
Clement <clem.chenebault@gmail.com>
2021-01-19 13:19:59 +0000

    camera

--- camera/assets/js/config.js
+++ camera/assets/js/config.js
@@ -1,7 +1,7 @@
 var WEBGL_CONFIG = {
   SHOW_LOADER_FAKE: false,
   MIN_LOADING_TIME: 0,
-  DELAY_FADEOUT_INFOS: 0,
+  DELAY_FADEOUT_INFOS: 4,
   ASSETS_PATH: "assets/",
   NAME_BUILD: "WebGLAnimation",
   SOUND: false,
@@ -12,5 +12,7 @@ var WEBGL_CONFIG = {
   COLOR: "white",
   BACKGROUND_COLOR:
     "linear-gradient(0deg, rgba(248,181,128,1) 0%, rgba(100,96,115,1) 40%, rgba(100,96,115,1) 60%, rgba(248,181,128,1) 100%)",
-  CREDITS: [],
+  CREDITS: [
+    "<a href='https://capturelandscapes.com/landscape-photography-in-arctic-norway/' target='_blank'>Photo</a> by Bernshausen Hendrik",
+  ],
 };
