a41c56891a530dd4a19b83992a387969209cc962
Clement <clem.chenebault@gmail.com>
2021-01-25 11:28:40 +0000

    fix image path

--- pixi-plane-demo/assets/js/config.js
+++ pixi-plane-demo/assets/js/config.js
@@ -2,18 +2,15 @@ var WEBGL_CONFIG = {
   SHOW_LOADER_FAKE: true,
   MIN_LOADING_TIME: 2,
   DELAY_FADEOUT_INFOS: 1000,
-  ASSETS_PATH: "assets/",
+  ASSETS_PATH: "./assets/",
   NAME_BUILD: "WebGLAnimation",
   SOUND: false,
   START_BUTTON: false,
   TITLE: "Pixi-Plane",
-  INSTRUCTIONS:
-    "Drag the points around the sprite",
-  DESCRIPTION:
-    "",
+  INSTRUCTIONS: "Drag the points around the sprite",
+  DESCRIPTION: "",
   COLOR: "white",
-  BACKGROUND_COLOR:
-    "#4287f5",
+  BACKGROUND_COLOR: "#4287f5",
   CREDITS: [
     "Package: <a href='https://github.com/madclem/pixi-plane' target='_blank'> pixi-plane </a>",
   ],
