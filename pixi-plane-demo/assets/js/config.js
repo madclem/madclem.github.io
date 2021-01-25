a5987b281b5f1fdc6340d74a3b47c1b71cb2397f
Clement <clem.chenebault@gmail.com>
2021-01-25 11:12:26 +0000

    pixi-plane-demo

---
+++ pixi-plane-demo/assets/js/config.js
@@ -0,0 +1,20 @@
+var WEBGL_CONFIG = {
+  SHOW_LOADER_FAKE: true,
+  MIN_LOADING_TIME: 2,
+  DELAY_FADEOUT_INFOS: 1000,
+  ASSETS_PATH: "assets/",
+  NAME_BUILD: "WebGLAnimation",
+  SOUND: false,
+  START_BUTTON: false,
+  TITLE: "Pixi-Plane",
+  INSTRUCTIONS:
+    "Drag the points around the sprite",
+  DESCRIPTION:
+    "",
+  COLOR: "white",
+  BACKGROUND_COLOR:
+    "#4287f5",
+  CREDITS: [
+    "Package: <a href='https://github.com/madclem/pixi-plane' target='_blank'> pixi-plane </a>",
+  ],
+};
