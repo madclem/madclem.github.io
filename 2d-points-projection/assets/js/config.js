1afe745d2c9b5d62ea98465d5e81c282f5366b37
Clement <clem.chenebault@gmail.com>
2021-01-27 00:08:09 +0000

    projections

---
+++ 2d-points-projection/assets/js/config.js
@@ -0,0 +1,16 @@
+var WEBGL_CONFIG = {
+  SHOW_LOADER_FAKE: true,
+  MIN_LOADING_TIME: 2,
+  DELAY_FADEOUT_INFOS: 4,
+  ASSETS_PATH: "assets/",
+  NAME_BUILD: "WebGLAnimation",
+  SOUND: false,
+  START_BUTTON: false,
+  TITLE: "2D: CAMERA AND 3D PROJECTION",
+  INSTRUCTIONS: "Toggle 2D / 3D with button, and move around",
+  DESCRIPTION: "2D scenes with 3D camera (CPU calculations)",
+  COLOR: "#2b2b2b",
+  BACKGROUND_COLOR:
+    "radial-gradient(circle, rgba(226,226,226,1) 0%, rgba(241,241,241,1) 100%)",
+  CREDITS: [],
+};
---
+++ 2d-scene-projection/assets/js/config.js
@@ -0,0 +1,20 @@
+var WEBGL_CONFIG = {
+  SHOW_LOADER_FAKE: true,
+  MIN_LOADING_TIME: 4,
+  DELAY_FADEOUT_INFOS: 4,
+  ASSETS_PATH: "assets/",
+  NAME_BUILD: "WebGLAnimation",
+  SOUND: false,
+  START_BUTTON: false,
+  TITLE: "2D: CAMERA + HOMOGRAPHIC TRANSFORM",
+  INSTRUCTIONS: "Touch your screen / move your mouse around",
+  DESCRIPTION:
+    "2D scenes with 3D projections (to calculate the points) and homographic transforms (texture projection)",
+  COLOR: "white",
+  BACKGROUND_COLOR:
+    "radial-gradient(circle, rgba(112,110,110,1) 0%, rgba(31,31,31,1) 100%)",
+  CREDITS: [
+    "Videos from <a href='https://www.videvo.net/' target='_blank'> videvi.net </a>",
+    "Package: <a href='https://github.com/madclem/pixi-plane' target='_blank'> pixi-plane </a>",
+  ],
+};
