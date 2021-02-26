b98e95fbfc45cb866eafeb56ec48108be7dced6f
Clement <clem.chenebault@gmail.com>
2021-02-26 00:13:05 +0000

    paper wip

---
+++ paper/assets/js/config.js
@@ -0,0 +1,19 @@
+var WEBGL_CONFIG = {
+  SHOW_LOADER_FAKE: true,
+  MIN_LOADING_TIME: 2,
+  DELAY_FADEOUT_INFOS: 4,
+  ASSETS_PATH: "assets/",
+  NAME_BUILD: "WebGLAnimation",
+  SOUND: false,
+  START_BUTTON: false,
+  TITLE: "Paper 01 - Displacement",
+  INSTRUCTIONS:
+    "Tap your screen",
+  DESCRIPTION:
+    "UV displacement on a render texture, from a Pixi.js scene",
+  COLOR: "black",
+  BACKGROUND_COLOR:
+    "white",
+  CREDITS: [
+  ],
+};
