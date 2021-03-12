d5d927b8345aad54249b2e89f1260e0eab5e75da
Clement <clem.chenebault@gmail.com>
2021-03-12 08:39:51 +0000

    frames: 01

---
+++ frames/01/assets/js/config.js
@@ -0,0 +1,16 @@
+var WEBGL_CONFIG = {
+  SHOW_LOADER_FAKE: false,
+  MIN_LOADING_TIME: 2,
+  DELAY_FADEOUT_INFOS: 0,
+  ASSETS_PATH: "assets/",
+  NAME_BUILD: "WebGLAnimation",
+  SOUND: false,
+  START_BUTTON: false,
+  TITLE: "Frames: 01",
+  INSTRUCTIONS: "",
+  DESCRIPTION: "Fragment shader composition",
+  COLOR: "white",
+  BACKGROUND_COLOR:
+    "radial-gradient(circle, rgba(19,170,170,1) 0%, rgba(176,230,243,1) 100%)",
+  CREDITS: [],
+};
