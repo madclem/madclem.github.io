bd1b7fe2fd22bd641bb3b37b54a9b4eecf9417d6
Clement <clem.chenebault@gmail.com>
2021-03-19 03:05:22 +0000

    audio-waves

---
+++ audio-waves/assets/js/config.js
@@ -0,0 +1,19 @@
+var WEBGL_CONFIG = {
+  SHOW_LOADER_FAKE: false,
+  MIN_LOADING_TIME: 2,
+  DELAY_FADEOUT_INFOS: 4,
+  ASSETS_PATH: "assets/",
+  NAME_BUILD: "WebGLAnimation",
+  SOUND: false,
+  START_BUTTON: false,
+  TITLE: "Audio waves animation (no audio)",
+  INSTRUCTIONS:
+    "",
+  DESCRIPTION:
+    "Fragment shader audio waves animation",
+  COLOR: "#a2a3a3",
+  BACKGROUND_COLOR:
+    "linear-gradient(0deg, rgba(0,0,0,1) 26%, rgba(14,45,61,1) 49%, rgba(0,0,0,1) 72%)",
+  CREDITS: [
+  ],
+};
