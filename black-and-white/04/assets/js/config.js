45a3e286ed7caafd8a09df668fddd9d6689f0204
Clement <clem.chenebault@gmail.com>
2021-03-26 09:58:43 +0000

    coral

---
+++ black-and-white/04/assets/js/config.js
@@ -0,0 +1,16 @@
+var WEBGL_CONFIG = {
+  SHOW_LOADER_FAKE: false,
+  MIN_LOADING_TIME: 2,
+  DELAY_FADEOUT_INFOS: 3,
+  ASSETS_PATH: "assets/",
+  NAME_BUILD: "WebGLAnimation",
+  SOUND: false,
+  START_BUTTON: false,
+  TITLE: "B&W - 04 - Coral",
+  INSTRUCTIONS: "",
+  DESCRIPTION: "",
+  COLOR: "#d4d4d4",
+  BACKGROUND_COLOR:
+    "radial-gradient(circle, rgba(54,54,54,1) 0%, rgba(40,39,39,1) 28%, rgba(0,0,0,1) 93%)",
+  CREDITS: [],
+};
