513f7a987ffbafbe903b53e0888a70ef1b766840
Clement <clem.chenebault@gmail.com>
2021-04-09 11:40:57 +0100

    cubes-dump

---
+++ wip/cubes-dump/assets/js/config.js
@@ -0,0 +1,19 @@
+var WEBGL_CONFIG = {
+  SHOW_LOADER_FAKE: true,
+  MIN_LOADING_TIME: 2,
+  DELAY_FADEOUT_INFOS: 4,
+  ASSETS_PATH: "assets/",
+  NAME_BUILD: "WebGLAnimation",
+  SOUND: false,
+  START_BUTTON: false,
+  TITLE: "Experiment: WIP - Cubes dump",
+  INSTRUCTIONS:
+    "",
+  DESCRIPTION:
+    "Experimenting with gradient borders and 3D in Pixi.js",
+  COLOR: "#111111",
+  BACKGROUND_COLOR:
+    "radial-gradient(circle, rgba(255,255,255,1) 0%, rgba(226,226,226,1) 100%)",
+  CREDITS: [
+  ],
+};
