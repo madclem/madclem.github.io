f79763c479497006a01526ba777a7dc9db090534
Clement <clem.chenebault@gmail.com>
2021-04-14 00:28:16 +0100

    playground

---
+++ playground/assets/js/config.js
@@ -0,0 +1,19 @@
+var WEBGL_CONFIG = {
+  SHOW_LOADER_FAKE: true,
+  MIN_LOADING_TIME: 2,
+  DELAY_FADEOUT_INFOS: 4,
+  ASSETS_PATH: "assets/",
+  NAME_BUILD: "WebGLAnimation",
+  SOUND: false,
+  START_BUTTON: false,
+  TITLE: "Experiment: Playground",
+  INSTRUCTIONS:
+    "Click / Tap your screen to change shapes. Press and move to rotate around.",
+  DESCRIPTION:
+    "Pixi.js 3D experimentations, shadows, phyics (cannon.js), fragment shader animation",
+  COLOR: "#111111",
+  BACKGROUND_COLOR:
+    "radial-gradient(circle, rgba(255,255,255,1) 0%, rgba(226,226,226,1) 100%)",
+  CREDITS: [
+  ],
+};
