d956b5c6bd9b54adebd4255a9883ce5a519951e0
Clement <clem.chenebault@gmail.com>
2021-04-28 11:01:55 +0100

    texts 02 upside down

---
+++ texts/02/assets/js/config.js
@@ -0,0 +1,19 @@
+var WEBGL_CONFIG = {
+  SHOW_LOADER_FAKE: true,
+  MIN_LOADING_TIME: 2,
+  DELAY_FADEOUT_INFOS: 4,
+  ASSETS_PATH: "assets/",
+  NAME_BUILD: "WebGLAnimation",
+  SOUND: false,
+  START_BUTTON: false,
+  TITLE: "Texts interface: Upside Down",
+  INSTRUCTIONS:
+    "Scroll / Drag - Move left and right to toggle the upside down",
+  DESCRIPTION:
+    "GLSL, Fragment animation, 3D, 2D, Pixi.js",
+  COLOR: "black",
+  BACKGROUND_COLOR:
+    "white",
+  CREDITS: [
+  ],
+};
