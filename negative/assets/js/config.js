52eeda1afa5545bc7ac8f5a133f893efb3d2b896
clement <clement@kindeo.com>
2022-10-11 17:45:50 +0200

    negative

---
+++ negative/assets/js/config.js
@@ -0,0 +1,16 @@
+var WEBGL_CONFIG = {
+  SHOW_LOADER_FAKE: false,
+  MIN_LOADING_TIME: 1,
+  DELAY_FADEOUT_INFOS: 4,
+  ASSETS_PATH: "assets/",
+  NAME_BUILD: "WebGLAnimation",
+  SOUND: false,
+  START_BUTTON: false,
+  TITLE: "Negative slide",
+  INSTRUCTIONS: "",
+  DESCRIPTION: "",
+  COLOR: "white",
+  BACKGROUND_COLOR:
+    "linear-gradient(0deg, rgba(22,22,22,1) 0%, rgba(45,45,45,1) 40%, rgba(46,46,46,1) 60%, rgba(23,23,23,1) 100%)",
+  CREDITS: [],
+};
