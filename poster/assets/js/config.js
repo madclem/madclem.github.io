ae17a87ac5d823e57a97e13d1cdd20f99537397e
clement <clement@kindeo.com>
2022-10-27 17:35:14 +0200

    poster

---
+++ poster/assets/js/config.js
@@ -0,0 +1,16 @@
+var WEBGL_CONFIG = {
+  SHOW_LOADER_FAKE: false,
+  MIN_LOADING_TIME: 1,
+  DELAY_FADEOUT_INFOS: 3,
+  ASSETS_PATH: "assets/",
+  NAME_BUILD: "WebGLAnimation",
+  SOUND: false,
+  START_BUTTON: false,
+  TITLE: "Title Text Poster",
+  INSTRUCTIONS: "",
+  DESCRIPTION: "",
+  COLOR: "black",
+  BACKGROUND_COLOR:
+    "radial-gradient(circle, rgba(255,255,255,1) 0%, rgba(153,236,227,1) 43%, rgba(41,148,137,1) 100%)",
+  CREDITS: [],
+};
