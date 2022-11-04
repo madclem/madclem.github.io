d47bb2104ed9b6595563cb3a5064e1fba4062a87
clement <clement@kindeo.com>
2022-11-04 14:26:18 +0100

    cakes

---
+++ kindeo-cakes/assets/js/config.js
@@ -0,0 +1,16 @@
+var WEBGL_CONFIG = {
+  SHOW_LOADER_FAKE: false,
+  MIN_LOADING_TIME: 1,
+  DELAY_FADEOUT_INFOS: 0,
+  ASSETS_PATH: "kindeo-cakes/assets/",
+  NAME_BUILD: "WebGLAnimation",
+  SOUND: false,
+  START_BUTTON: false,
+  TITLE: "Cakes!!!",
+  INSTRUCTIONS: "",
+  DESCRIPTION: "",
+  COLOR: "black",
+  BACKGROUND_COLOR:
+    "radial-gradient(circle, rgba(255,255,255,1) 0%, rgba(236,204,153,1) 43%, rgba(41,148,137,1) 100%)",
+  CREDITS: [],
+};
