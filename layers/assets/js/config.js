dbfcb81a50322939f98b0b4ff51fc984b407b452
clement <clement@kindeo.com>
2022-11-16 17:47:29 +0100

    layers

---
+++ layers/assets/js/config.js
@@ -0,0 +1,15 @@
+var WEBGL_CONFIG = {
+  SHOW_LOADER_FAKE: false,
+  MIN_LOADING_TIME: 2,
+  DELAY_FADEOUT_INFOS: 0,
+  ASSETS_PATH: "./assets/",
+  NAME_BUILD: "WebGLAnimation",
+  SOUND: false,
+  START_BUTTON: false,
+  TITLE: "Layers - Christmas version",
+  INSTRUCTIONS: "",
+  DESCRIPTION: "",
+  COLOR: "white",
+  BACKGROUND_COLOR: "rgb(0, 24, 60)",
+  CREDITS: [],
+};
