6ea2875c21bd4c9bd0aa3f8a8badb13b9b4192ed
clement <clement@kindeo.com>
2023-03-07 02:06:14 +0100

    mothersday

---
+++ kindeo-mothersday/assets/js/config.js
@@ -0,0 +1,16 @@
+var WEBGL_CONFIG = {
+  SHOW_LOADER_FAKE: false,
+  MIN_LOADING_TIME: 1,
+  DELAY_FADEOUT_INFOS: 0,
+  ASSETS_PATH: "assets/",
+  NAME_BUILD: "WebGLAnimation",
+  SOUND: false,
+  START_BUTTON: false,
+  TITLE: "Mother's day",
+  INSTRUCTIONS: "",
+  DESCRIPTION: "",
+  COLOR: "white",
+  BACKGROUND_COLOR:
+    "radial-gradient(circle, rgba(238,174,202,1) 0%, rgba(148,187,233,1) 100%)",
+  CREDITS: [],
+};
