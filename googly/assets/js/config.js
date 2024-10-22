58be3f55250b8546d659e367c96c9302f9b9e7df
clement <clement@kindeo.com>
2024-10-22 14:23:11 +0200

    feat: googly eyes

---
+++ googly/assets/js/config.js
@@ -0,0 +1,16 @@
+var WEBGL_CONFIG = {
+  SHOW_LOADER_FAKE: false,
+  MIN_LOADING_TIME: 1,
+  DELAY_FADEOUT_INFOS: 3,
+  ASSETS_PATH: "assets/",
+  NAME_BUILD: "WebGLAnimation",
+  SOUND: false,
+  START_BUTTON: false,
+  TITLE: "Googly 👀",
+  INSTRUCTIONS: "",
+  DESCRIPTION: "",
+  COLOR: "black",
+  BACKGROUND_COLOR:
+    "radial-gradient(circle, rgba(255,255,255,1) 0%, rgba(222,215,255,1) 100%)",
+  CREDITS: [],
+};
