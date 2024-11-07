59bc4620d1fcb08c79285d6e8593fbca501d45bd
clement <clement@kindeo.com>
2024-11-07 15:26:11 +0100

    feat: bubbly transitions

---
+++ bubbly-transition/assets/js/config.js
@@ -0,0 +1,19 @@
+var WEBGL_CONFIG = {
+  SHOW_LOADER_FAKE: true,
+  MIN_LOADING_TIME: 2,
+  DELAY_FADEOUT_INFOS: 2,
+  ASSETS_PATH: "assets/",
+  NAME_BUILD: "WebGLAnimation",
+  SOUND: false,
+  START_BUTTON: false,
+  TITLE: "Bubbly Transitions",
+  INSTRUCTIONS:
+    "Arrow Left | Right to transition",
+  DESCRIPTION:
+    "Bubbly Transition using 2D geometries",
+  COLOR: "white",
+  BACKGROUND_COLOR:
+    "linear-gradient(90deg, rgba(103,211,204,1) 0%, rgba(114,216,174,1) 100%)",
+  CREDITS: [
+  ],
+};
---
+++ bubbly-transition/assets/js/gl.es.js
@@ -0,0 +1,1 @@
+export { cV as app, cX as default, cW as renderer } from "./MainApp.js";
